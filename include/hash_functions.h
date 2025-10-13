d481fa139c352d04488955fdf7d13918841d9e24
Dovhal Illia <dovhal.illia21@gmail.com>
2025-10-13 16:59:02 +0200

    refactor(hash functions): update function calls

--- include/hash_functions.h
+++ include/hash_functions.h
@@ -1,12 +1,39 @@
 #pragma once
 #include <cstddef>
-#include <iostream>
 #include <cstdint>
 #include <cstring>
 
-class HashFunctions {
-public:
-    static size_t hash(int key);
-    static size_t hash(char key);
-    static size_t hash(float key);
+template <typename T> struct Hash {
+  size_t operator()(const T &key) const;
+};
+
+template <> struct Hash<int> {
+  size_t operator()(int key) const { return key * 2654435761u; }
+};
+
+template <> struct Hash<char> {
+  size_t operator()(int key) const {
+    int hash = static_cast<int>(key << 8);
+    hash *= 0xF5;
+    return hash * 2654435761u;
+  }
+};
+
+template <> struct Hash<float> {
+  size_t operator()(int key) const {
+    if (key == 0.0f)
+      key = 0.0f;
+    uint32_t bits;
+    std::memcpy(&bits, &key, sizeof(bits));
+
+    bits ^= bits >> 17;
+    bits *= 0xEF59A5B3;
+    bits ^= bits >> 11;
+    bits *= 0xAC4C1B51;
+    bits ^= bits >> 15;
+    bits *= 0x31848BAB;
+    bits ^= bits >> 14;
+
+    return static_cast<size_t>(bits);
+  }
 };
