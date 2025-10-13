9b00297104a3a4d21111b79200db2bb987c6d3e4
Dovhal Illia <dovhal.illia21@gmail.com>
2025-10-13 17:03:51 +0200

    feat(hash table): add EntryState and implement insert and rehash

--- include/open_addressing_hash_table.h
+++ include/open_addressing_hash_table.h
@@ -1,15 +1,17 @@
 #pragma once
-#include <vector>
 #include "hash_functions.h"
+#include <vector>
 
-template <typename K, typename V> struct Entry {
+enum class EntryState { EMPTY, OCCUPIED, DELETED };
+
+template <typename K, typename V> //
+struct Entry {
   K key;
   V value;
-  bool occupied;
-  bool deleted;
+  EntryState state = EntryState::EMPTY;
 };
 
-template <typename K, typename V> 
+template <typename K, typename V> //
 class OpenAddressingHashTable {
 public:
   OpenAddressingHashTable();
@@ -18,22 +20,34 @@ public:
   V *find(K key);
 
   void rehash(size_t new_capacity);
+  std::vector<Entry<K, V>> get_container() const;
 
 private:
   std::vector<Entry<K, V>> data;
-  int size;
+  Hash<K> hasher;
+  size_t num_elements = 0;
 };
 
 template <typename K, typename V>
-OpenAddressingHashTable<K, V>::OpenAddressingHashTable() : size(0) {
-  data.resize(4);
-}
+OpenAddressingHashTable<K, V>::OpenAddressingHashTable()
+    : num_elements(0), data(4) {}
 
 template <typename K, typename V>
 void OpenAddressingHashTable<K, V>::insert(K key, V value) {
-  if ((float)size / data.size() > 0.7f) {
+  if ((float)num_elements / data.size() > 0.7f) {
     rehash(data.size() * 2);
   }
+  int index = hasher(key) & (data.size() - 1);
+
+  while (data[index].state == EntryState::OCCUPIED) {
+    index = (index + 1) & (data.size() - 1);
+  }
+
+  data[index].value = value;
+  data[index].key = key;
+  data[index].state = EntryState::OCCUPIED;
+  num_elements++;
+  return;
 }
 
 template <typename K, typename V>
@@ -45,9 +59,23 @@ V *OpenAddressingHashTable<K, V>::find(K key) {}
 template <typename K, typename V>
 void OpenAddressingHashTable<K, V>::rehash(size_t new_capacity) {
   std::vector<Entry<K, V>> new_data(new_capacity);
-  for (int i = 0; i < data.size(); ++i) {
-    // if (data[i].occupied && !data[i].deleted) {
-    //   size_t new_index
-    // }
+  for (size_t i = 0; i < data.size(); ++i) {
+    if (data[i].state == EntryState::OCCUPIED) {
+      size_t new_index = hasher(data[i].key) & (new_data.size() - 1);
+
+      while (new_data[new_index].state == EntryState::OCCUPIED) {
+        new_index = (new_index + 1) & (new_data.size() - 1);
+      }
+
+      new_data[new_index].key = data[i].key;
+      new_data[new_index].value = data[i].value;
+      new_data[new_index].state = EntryState::OCCUPIED;
+    }
   }
+  data = std::move(new_data);
+}
+
+template <typename K, typename V>
+std::vector<Entry<K, V>> OpenAddressingHashTable<K, V>::get_container() const {
+  return data;
 }
--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
+#include "open_addressing_hash_table.h"
+#include <iostream>
 //cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && cmake --build build && ./build/OpenHashTable
+
 int main (int argc, char *argv[]) {
+  OpenAddressingHashTable<int, char> table;
+  table.insert(4, 'c');
+  table.insert(-11, 'a');
+  table.insert(5, 'b');
+  table.insert(40, 'h');
+  table.insert(80, 'm');
+  std::cout << table.get_container().size() << std::endl;
+  for(auto& x : table.get_container()) {
+    std::cout << x.value << " ";
+  }
+  std::cout << std::endl;
   return 0;
 }
