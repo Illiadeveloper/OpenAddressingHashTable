9298b563c7ac9115b0e8360ebd9e656ff2f11f94
Dovhal Illia <dovhal.illia21@gmail.com>
2025-10-14 13:42:52 +0200

    feat(hash table): add iterator class and begin() and end() methods

--- include/open_addressing_hash_table.h
+++ include/open_addressing_hash_table.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "hash_functions.h"
+#include <cstddef>
+#include <iterator>
+#include <locale>
 #include <vector>
 
 enum class EntryState { EMPTY, OCCUPIED, DELETED };
@@ -14,13 +17,73 @@ struct Entry {
 template <typename K, typename V> //
 class OpenAddressingHashTable {
 public:
-  OpenAddressingHashTable();
-  void insert(K key, V value);
-  void erase(K key);
-  V *find(K key);
+  class iterator {
+  public:
+    using iterator_category = std::forward_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = Entry<K, V>;
+    using pointer = value_type *;
+    using reference = value_type &;
 
-  void rehash(size_t new_capacity);
-  std::vector<Entry<K, V>> get_container() const;
+    iterator(pointer ptr, pointer end_ptr) : current(ptr), end(end_ptr) {}
+
+    iterator operator++() {
+      ++current;
+      skip_empty();
+      return *this;
+    };
+
+    iterator operator++(int) {
+      pointer temp = *this;
+      ++current;
+      skip_empty();
+      return temp;
+    };
+
+    bool operator==(const iterator &other) { return current == other.current; }
+    bool operator!=(const iterator &other) { return !(*this == other); }
+
+    reference operator*() const { return *current; }
+    pointer operator->() const { return current; };
+
+  private:
+    pointer current;
+    pointer end;
+
+    void skip_empty() {
+      while (current != end && current->state != EntryState::OCCUPIED) {
+        ++current;
+      }
+    }
+  };
+
+  class const_iterator {};
+
+  // Usings for STD cointainers
+  using key_type = K;
+  using mapped_type = V;
+  using value_type = Entry<K, V>;
+  using size_type = size_t;
+  using difference_type = std::ptrdiff_t;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  using pointer = value_type *;
+  using const_pointer = const value_type *;
+
+  using iterator = iterator;
+
+  OpenAddressingHashTable() : num_elements(0), data(4) {}
+
+  iterator begin() { return iterator(&data.front(), &data.back() + 1); };
+  iterator end() { return iterator(&data.back() + 1, &data.back() + 1); };
+
+  void insert(key_type key, mapped_type value);
+  void erase(key_type key);
+  iterator find(key_type key);
+
+  void rehash(size_type new_capacity);
+
+  std::vector<Entry<K, V>> get_container() const { return data; }
 
 private:
   std::vector<Entry<K, V>> data;
@@ -29,11 +92,7 @@ private:
 };
 
 template <typename K, typename V>
-OpenAddressingHashTable<K, V>::OpenAddressingHashTable()
-    : num_elements(0), data(4) {}
-
-template <typename K, typename V>
-void OpenAddressingHashTable<K, V>::insert(K key, V value) {
+void OpenAddressingHashTable<K, V>::insert(key_type key, mapped_type value) {
   if ((float)num_elements / data.size() > 0.7f) {
     rehash(data.size() * 2);
   }
@@ -51,13 +110,14 @@ void OpenAddressingHashTable<K, V>::insert(K key, V value) {
 }
 
 template <typename K, typename V>
-void OpenAddressingHashTable<K, V>::erase(K key) {}
+void OpenAddressingHashTable<K, V>::erase(key_type key) {}
 
 template <typename K, typename V>
-V *OpenAddressingHashTable<K, V>::find(K key) {}
+typename OpenAddressingHashTable<K, V>::iterator
+OpenAddressingHashTable<K, V>::find(key_type key) {}
 
 template <typename K, typename V>
-void OpenAddressingHashTable<K, V>::rehash(size_t new_capacity) {
+void OpenAddressingHashTable<K, V>::rehash(size_type new_capacity) {
   std::vector<Entry<K, V>> new_data(new_capacity);
   for (size_t i = 0; i < data.size(); ++i) {
     if (data[i].state == EntryState::OCCUPIED) {
@@ -73,9 +133,4 @@ void OpenAddressingHashTable<K, V>::rehash(size_t new_capacity) {
     }
   }
   data = std::move(new_data);
-}
-
-template <typename K, typename V>
-std::vector<Entry<K, V>> OpenAddressingHashTable<K, V>::get_container() const {
-  return data;
 }
--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include "open_addressing_hash_table.h"
 #include <iostream>
+#include <unordered_map>
 //cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && cmake --build build && ./build/OpenHashTable
 
 int main (int argc, char *argv[]) {
   OpenAddressingHashTable<int, char> table;
-  table.insert(4, 'c');
+  table.insert(5, 'c');
   table.insert(-11, 'a');
   table.insert(5, 'b');
   table.insert(40, 'h');
   table.insert(80, 'm');
-  std::cout << table.get_container().size() << std::endl;
+
+  for(auto& x : table) {
+    std::cout << x.value << " ";
+  }
+  std::cout << std::endl;
   for(auto& x : table.get_container()) {
     std::cout << x.value << " ";
   }
